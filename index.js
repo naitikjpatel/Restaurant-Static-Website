30a82a9f18c9ce1c01be761dc5bfd49fbdd160d5
Naitik Patel <120157810+naitikjpatel@users.noreply.github.com>
2024-02-29 07:09:45 +0530

    Added Require files

---
+++ index.js
@@ -0,0 +1,217 @@
+// countValue element che  ->countValue.innerText e value ape che te const nathi etle
+const countValue1=document.querySelector("#counter1");
+const m11=document.getElementById("m1");
+const p11=document.getElementById("p1");
+m11.addEventListener("click",()=>{
+    //get the value from ui
+    let value=parseInt(countValue1.innerText); //string to int ma conver
+    //update the value
+    if(value>=1)
+    value=value-1;
+    //set the value onto ui
+    countValue1.innerText=value;
+});
+p11.addEventListener("click",()=>{
+    //get the value from ui
+    let value=parseInt(countValue1.innerText); //string to int ma conver
+    //update the value
+    value=value+1;
+    //set the value onto ui
+    countValue1.innerText=value;
+    // console.log(document.getElementById('counter1').value);
+});
+
+
+
+const countValue2=document.querySelector("#counter2");
+const m12=document.getElementById("m2");
+const p12=document.getElementById("p2");
+m12.addEventListener("click",()=>{
+    //get the value from ui
+    let value=parseInt(countValue2.innerText); //string to int ma conver
+    //update the value
+    if(value>=1)
+    value=value-1;
+    //set the value onto ui
+    countValue2.innerText=value;
+});
+p12.addEventListener("click",()=>{
+    //get the value from ui
+    let value=parseInt(countValue2.innerText); //string to int ma conver
+    //update the value
+    value=value+1;
+    //set the value onto ui
+    countValue2.innerText=value;
+});
+
+const countValue3=document.querySelector("#counter3");
+const m13=document.getElementById("m3");
+const p13=document.getElementById("p3");
+m13.addEventListener("click",()=>{
+    //get the value from ui
+    let value=parseInt(countValue3.innerText); //string to int ma conver
+    //update the value
+    if(value>=1)
+    value=value-1;
+    //set the value onto ui
+    countValue3.innerText=value;
+});
+p13.addEventListener("click",()=>{
+    //get the value from ui
+    let value=parseInt(countValue3.innerText); //string to int ma conver
+    //update the value
+    value=value+1;
+    //set the value onto ui
+    countValue3.innerText=value;
+});
+
+const countValue4=document.querySelector("#counter4");
+const m14=document.getElementById("m4");
+const p14=document.getElementById("p4");
+m14.addEventListener("click",()=>{
+    //get the value from ui
+    let value=parseInt(countValue4.innerText); //string to int ma conver
+    //update the value
+    if(value>=1)
+    value=value-1;
+    //set the value onto ui
+    countValue4.innerText=value;
+});
+p14.addEventListener("click",()=>{
+    //get the value from ui
+    let value=parseInt(countValue4.innerText); //string to int ma conver
+    //update the value
+    value=value+1;
+    //set the value onto ui
+    countValue4.innerText=value;
+});
+
+const countValue5=document.querySelector("#counter5");
+const m15=document.getElementById("m5");
+const p15=document.getElementById("p5");
+m15.addEventListener("click",()=>{
+    //get the value from ui
+    let value=parseInt(countValue5.innerText); //string to int ma conver
+    //update the value
+    if(value>=1)
+    value=value-1;
+    //set the value onto ui
+    countValue5.innerText=value;
+});
+p15.addEventListener("click",()=>{
+    //get the value from ui
+    let value=parseInt(countValue5.innerText); //string to int ma conver
+    //update the value
+    value=value+1;
+    //set the value onto ui
+    countValue5.innerText=value;
+});
+
+const countValue6=document.querySelector("#counter6");
+const m16=document.getElementById("m6");
+const p16=document.getElementById("p6");
+m16.addEventListener("click",()=>{
+    //get the value from ui
+    let value=parseInt(countValue6.innerText); //string to int ma conver
+    //update the value
+    if(value>=1)
+    value=value-1;
+    //set the value onto ui
+    countValue6.innerText=value;
+});
+p16.addEventListener("click",()=>{
+    //get the value from ui
+    let value=parseInt(countValue6.innerText); //string to int ma conver
+    //update the value
+    value=value+1;
+    //set the value onto ui
+    countValue6.innerText=value;
+});
+
+
+const countValue7=document.querySelector("#counter7");
+const m17=document.getElementById("m7");
+const p17=document.getElementById("p7");
+m17.addEventListener("click",()=>{
+    //get the value from ui
+    let value=parseInt(countValue7.innerText); //string to int ma conver
+    //update the value
+    if(value>=1)
+    value=value-1;
+    //set the value onto ui
+    countValue7.innerText=value;
+});
+p17.addEventListener("click",()=>{
+    //get the value from ui
+    let value=parseInt(countValue7.innerText); //string to int ma conver
+    //update the value
+    value=value+1;
+    //set the value onto ui
+    countValue7.innerText=value;
+});
+
+
+const countValue9=document.querySelector("#counter9");
+const m19=document.getElementById("m9");
+const p19=document.getElementById("p9");
+m19.addEventListener("click",()=>{
+    //get the value from ui
+    let value=parseInt(countValue9.innerText); //string to int ma conver
+    //update the value
+    if(value>=1)
+    value=value-1;
+    //set the value onto ui
+    countValue9.innerText=value;
+});
+p19.addEventListener("click",()=>{
+    //get the value from ui
+    let value=parseInt(countValue9.innerText); //string to int ma conver
+    //update the value
+    value=value+1;
+    //set the value onto ui
+    countValue9.innerText=value;
+});
+
+const countValue8=document.querySelector("#counter8");
+const m18=document.getElementById("m8");
+const p18=document.getElementById("p8");
+m18.addEventListener("click",()=>{
+    //get the value from ui
+    let value=parseInt(countValue8.innerText); //string to int ma conver
+    //update the value
+    if(value>=1)
+    value=value-1;
+    //set the value onto ui
+    countValue8.innerText=value;
+});
+p18.addEventListener("click",()=>{
+    //get the value from ui
+    let value=parseInt(countValue8.innerText); //string to int ma conver
+    //update the value
+    value=value+1;
+    //set the value onto ui
+    countValue8.innerText=value;
+});
+
+
+
+
+const increment=()=>{
+    //get the value from ui
+    let value=parseInt(countValue.innerText); //string to int ma conver
+    //update the value
+    value=value+1;
+    //set the value onto ui
+    countValue.innerText=value;
+};
+const decrement=()=>{
+    //get the value from ui
+    let value=parseInt(countValue.innerText); //string to int ma conver
+    //update the value
+    if(value>=1)
+    value=value-1;
+    //set the value onto ui
+    countValue.innerText=value;
+};
+
+
